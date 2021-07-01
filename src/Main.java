f89ff47cdb9ff6b400afcee31678d5795766ff42
Malgorzata Rog <Malgorzata.Rog@softwire.com>
2021-07-01 10:58:13 +0100

    Changed method to use of regular expressions.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int counter = 0;
         Path emailFile = Path.of("sample.txt");
         String contentOfFile = Files.readString(emailFile);
-        for(int i = 0; i < contentOfFile.length()-13; i++)
-        {
-            if ("@softwire.com".equals(contentOfFile.substring(i, i+13))) {counter ++;}
+        String patternString = "@softwire.com";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(contentOfFile);
+        while(matcher.find()) {
+            counter++;
         }
         System.out.println(counter);
     }
