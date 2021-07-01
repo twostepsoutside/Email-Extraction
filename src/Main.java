08d94dca26461930f580d795a38bc5ec88b2de86
Malgorzata Rog <Malgorzata.Rog@softwire.com>
2021-07-01 11:47:56 +0100

    Added HashMap to find all email adresses and the number of occurences of eah of the domains.

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,28 @@ import java.nio.file.Path;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int counter = 0;
         Path emailFile = Path.of("sample.txt");
         String contentOfFile = Files.readString(emailFile);
-        String patternString = "@softwire.com";
+        HashMap<String, Integer> adresses = new HashMap<>();
+
+        String patternString = "@(.+?)[ \n]";
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(contentOfFile);
-        while(matcher.find()) {
-            counter++;
+        while (matcher.find()) {
+            if (adresses.containsKey(matcher.group(1))) {
+                adresses.put(matcher.group(1), adresses.get(matcher.group(1)) + 1);
+            } else {
+                adresses.put(matcher.group(1), 1);
+            }
         }
-        System.out.println(counter);
+        adresses.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        });
     }
 
 }
